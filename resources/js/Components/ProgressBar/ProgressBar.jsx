de362c35e7729221bbcbb4499d54454c6828deed
Fernando Cueto <fernandoecueto@gmail.com>
2022-11-24 23:36:43 +0100

    progress bar component

---
+++ resources/js/Components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ProgressBar({ progressPercentage }) {
+  return (
+    <div className='h-2 w-full bg-gray-300'>
+        <div
+            style={{ width: `${progressPercentage}%`}}
+            className={`h-full ${
+                progressPercentage < 70 ? 'bg-red-600' : 'bg-green-600'}`}>
+        </div>
+    </div>
+  )
+}
